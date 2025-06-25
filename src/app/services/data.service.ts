4cea10e00a32c08bc9d60a7ddcbdc5e41ed98765
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-06-25 17:35:25 +0200

    Hauptliste funktioniert soweit

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 export interface VerlieheneSache {
+  id       : number;
   sache    : string,
   entleiher: string;
   frist    : string;
@@ -13,32 +14,36 @@ export class DataService {
 
   public verlieheneSachenArray: VerlieheneSache[] = [
     {
+      id       : 0,
       sache    : "Quietsche-Entchen",
       entleiher: "Alex",
       frist    : "2025-10-01"
     },
     {
+      id       : 1,
       sache    : "Wackel-Dackel",
       entleiher: "Berta",
       frist    : "2025-07-15"
     },
     {
+      id       : 2,
       sache    : "Gartenzwerg",
       entleiher: "Claus",
       frist    : "2025-08-20"
     },
     {
+      id       : 3,
       sache    : "Plömpel",
       entleiher: "Dora",
       frist    : "2025-09-10"
     },
     {
+      id       : 4,
       sache    : "Eierharfe",
       entleiher: "Erika",
       frist    : "2025-11-05"
     }
   ];
-
 
 
   public getVerlieheneSachen(): VerlieheneSache[] {
@@ -50,4 +55,5 @@ export class DataService {
 
     return this.verlieheneSachenArray[idx];
   }
+
 }
