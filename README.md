f019367ef3cdcdc5de381d96a64f5fcc7822793d
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-06-25 20:54:01 +0200

    README+LICENSE dazu; in Liste wird ein ueberfaelliges Objekt rot markiert

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ionic-App "Verleihliste #
+
+<br>
+
+Dieses Repo enthält eine Angular/Ionic-App, die die Verwendung des Ionic-Templates "list" demonstriert.
+
+<br>
+
+----
+
+## License ##
+
+<br>
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License).
+
+<br>
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,6 +5,7 @@ export interface VerlieheneSache {
   sache    : string,
   entleiher: string;
   frist    : string;
+  faellig  : boolean;
 }
 
 @Injectable({
@@ -17,31 +18,36 @@ export class DataService {
       id       : 0,
       sache    : "Eierharfe",
       entleiher: "Erika",
-      frist    : "05.11.2025"
+      frist    : "05.11.2025",
+      faellig  : false
     },
     {
       id       : 1,
       sache    : "Gartenzwerg",
       entleiher: "Claus",
-      frist    : "20.08.2025"
+      frist    : "20.08.2025",
+      faellig  : false
     },
     {
       id       : 2,
       sache    : "Plömpel",
       entleiher: "Dora",
-      frist    : "10.09.2025"
+      frist    : "10.06.2025",
+      faellig  : true
     },
     {
       id       : 3,
       sache    : "Quietsche-Entchen",
       entleiher: "Alex",
-      frist    : "Dauerleihgabe"
+      frist    : "Dauerleihgabe",
+      faellig  : false
     },
     {
       id       : 4,
       sache    : "Wackel-Dackel",
       entleiher: "Berta",
-      frist    : "15.07.2025"
+      frist    : "15.06.2026",
+      faellig  : false
     }
   ];
 
